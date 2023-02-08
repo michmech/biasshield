998e9ad2ec70f0569e1e3834cedf75b5c1a03772
Michal Měchura <michmech@lexiconista.com>
2023-02-08 13:13:31 +0100

    debiasbyus features are complete

--- pimp/pimp.js
+++ pimp/pimp.js
@@ -1,4 +1,5 @@
 const DeBiasByUs={
+  //IDs of source and target languages supported by debiasbyus: 
   langs: {
     "ar": 1,
     "zh": 2,
@@ -23,11 +24,15 @@ const DeBiasByUs={
     "tr": 19,
     "uk": 20,
   },
+
+  //IDs of sites as understood by debiasbyus:
   sites: {
     "google": 1,
     "deepl": 2,
   },
-  composeUrl: function(){
+
+  //URL at which to report a biased translation, with some fields prefilled:
+  composeSubmitUrl: function(){
     let url=`https://debiasbyus.ugent.be/share/`;
     url+=`?srcLang=${DeBiasByUs.langs[Pimp.lastScrapeResult.srcLang]}`;
     url+=`&srcText=${encodeURIComponent(Pimp.lastScrapeResult.srcText)}`;
@@ -36,23 +41,71 @@ const DeBiasByUs={
     url+=`&site=${DeBiasByUs.sites[Pimp.siteName]}`;
     return url;    
   },
+
+  //URL at which to lookup a previously reported biased translation:
+  composeLookupUrl: function(){
+    let url=`https://debiasbyus.ugent.be/lookup/`;
+    url+=`?srcLang=${DeBiasByUs.langs[Pimp.lastScrapeResult.srcLang]}`;
+    url+=`&srcText=${encodeURIComponent(Pimp.lastScrapeResult.srcText)}`;
+    url+=`&trgLang=${DeBiasByUs.langs[Pimp.lastScrapeResult.trgLang]}`;
+    url+=`&trgText=${encodeURIComponent(Pimp.lastScrapeResult.trgText)}`;
+    return url;    
+  },
+
+  //check whether the current translation has already been reported as biased:
+  check: function(){
+    window.setTimeout(function(){
+      const isBiased=(Pimp.lastScrapeResult.srcLang=="en" && Pimp.lastScrapeResult.srcText=="I need a doctor." && Pimp.lastScrapeResult.trgLang=="de" && Pimp.lastScrapeResult.trgText=="Ich brauche einen Arzt.");
+      const unbiasedVersion="Ich brauche ärtzliche Hilfe."; 
+      if(isBiased) {
+        Pimp.setState("debiasbyus", "alreadyReported", true);
+        Pimp.el.querySelector("a.lookupReport").href=DeBiasByUs.composeLookupUrl();
+        Pimp.el.querySelector("span.unbias").style.display="inline";
+        Pimp.el.querySelector("span.ununbias").style.display="none";
+        Pimp.el.querySelector("span.unbias").addEventListener("click", function(){
+          Pimp.injectTranslation(unbiasedVersion);
+          Pimp.el.querySelector("span.unbias").style.display="none";
+          Pimp.el.querySelector("span.ununbias").style.display="inline";
+        });
+        Pimp.el.querySelector("span.ununbias").addEventListener("click", function(){
+          Pimp.injectTranslation(Pimp.lastScrapeResult.trgText);
+          Pimp.el.querySelector("span.unbias").style.display="inline";
+          Pimp.el.querySelector("span.ununbias").style.display="none";
+        });
+      } else {
+        Pimp.setState("debiasbyus", "notYetReported", false);
+        Pimp.el.querySelector("a.submitReport").href=DeBiasByUs.composeSubmitUrl();
+      }
+    }, 1000);
+  },
 };
 
 const Fairslator={
-  srcLangs: ["en"],
-  trgLangs: ["de", "fr", "cs", "ga"],
+  //language pairs supported by fairslator:
+  langPairs: {
+    "en-de": true,
+    "en-fr": true,
+    "en-cs": true,
+    "en-ga": true,
+  },
 };
 
 const Pimp={
+  //our DOM element <div class="pimp"> at the bottom of the screen:
   el: null,
-  siteName: "",
 
+  //which site are we on?
+  siteName: "", //"google" or "deepl"
+
+  //switch to a tab ("debiasbyus" or "fairslator"): 
   switchTab: function(nick){
     Pimp.el.querySelectorAll(".tabs > .tab").forEach(el => {el.classList.remove("current")});
     Pimp.el.querySelector(`.tabs > .tab.${nick}`).classList.add("current");
     Pimp.el.querySelectorAll(".tabBodies > .tabBody").forEach(el => {el.style.display="none"});
     Pimp.el.querySelector(`.tabBodies > .tabBody.${nick}`).style.display="block";
   },
+
+  //set the state of a tab (whoNick = "debiasbyus" or "fairslator"): 
   setState: function(whoNick, stateNick, isTabLitUp){
     Pimp.el.querySelectorAll(`.tabBody.${whoNick} .state`).forEach(el => { el.style.display="none"; });
     Pimp.el.querySelectorAll(`.tabBody.${whoNick} .state.${stateNick}`).forEach(el => { el.style.display="block"; });
@@ -60,8 +113,14 @@ const Pimp={
     else Pimp.el.querySelector(`.tab.${whoNick}`).classList.add("off");
   },
 
+  //tell us what's on the screen:
   scrapeScreen: function(){
-    const ret={};
+    const ret={
+      srcLang: "",
+      srcText: "",
+      trgLang: "",
+      trgText: "",
+    };
     if(this.siteName=="google"){
       const params=new URLSearchParams(window.location.search);
       if(params && params.get) ret.srcLang=params.get("sl");
@@ -78,30 +137,36 @@ const Pimp={
     return ret;
   },
 
+  //called every two seconds to see if the screen state has changed:
   lastScrapeResult: {},
   check: function(){
     console.log("checking...");
     try{
       const scrapeResult=Pimp.scrapeScreen();
-      if(JSON.stringify(scrapeResult)==JSON.stringify(Pimp.lastScrapeResult)){
+      if(scrapeResult.srcLang==Pimp.lastScrapeResult.srcLang
+      && scrapeResult.srcText==Pimp.lastScrapeResult.srcText
+      && scrapeResult.trgLang==Pimp.lastScrapeResult.trgLang
+      && (scrapeResult.trgText==Pimp.lastScrapeResult.trgText || scrapeResult.trgText==Pimp.injectedTranslation)
+      ){
         console.log("no change");
       } else {
         console.log("yes change", scrapeResult);
         Pimp.lastScrapeResult=scrapeResult;
-        //determine the state of DeBiasByUs:
-        console.log(DeBiasByUs.langs[scrapeResult.srcLang]);
+        
+        //determine the state of debiasbyus:
         if(!scrapeResult.trgText){
           Pimp.setState("debiasbyus", "noTranslation", false);
         } else if(!DeBiasByUs.langs[scrapeResult.srcLang] || !DeBiasByUs.langs[scrapeResult.trgLang]) {
           Pimp.setState("debiasbyus", "unsupportedLanguagPair", false);
         } else {
-          Pimp.setState("debiasbyus", "offerToReport", true);
-          Pimp.el.querySelector("a.submitReport").href=DeBiasByUs.composeUrl();
+          Pimp.setState("debiasbyus", "checking", false);
+          DeBiasByUs.check();
         }
-        //determine the state of Fairslator:
+        
+        //determine the state of fairslator:
         if(!scrapeResult.trgText){
           Pimp.setState("fairslator", "noTranslation", false);
-        } else if(Fairslator.srcLangs.indexOf(scrapeResult.srcLang)==-1 || Fairslator.trgLangs.indexOf(scrapeResult.trgLang)==-1) {
+        } else if(!Fairslator.langPairs[`${scrapeResult.srcLang}-${scrapeResult.trgLang}`]) {
           Pimp.setState("fairslator", "unsupportedLanguagPair", false);
         } else {
           Pimp.setState("fairslator", "detectingAmbiguities", false);
@@ -113,22 +178,31 @@ const Pimp={
     }
   },
 
+  //insert a translation into the screen:
+  injectedTranslation: "",
   injectTranslation: function(text){
     if(this.siteName=="google"){
       trgText=document.querySelector("span.ryNqvb").innerText=text;
     }
     else if(this.siteName=="deepl"){
-      document.querySelector("textarea.lmt__target_textarea").value=text;
+      document.querySelectorAll("#target-dummydiv").forEach(el => {el.textContent=text});
+      document.querySelectorAll("section.lmt__side_container--target p").forEach(el => {el.textContent=text});
     }
+    Pimp.injectedTranslation=text;
   }
 };
+//everything beyond this point runs once when the extension is being loaded
 
+//discover which site we are on, google or deepl:
 if(/translate\.google\.com$/.test(window.location.hostname)) Pimp.siteName="google";
 else if(/www\.deepl\.com$/.test(window.location.hostname)) Pimp.siteName="deepl";
 
+//create our DOM element:
 document.querySelectorAll("div.pimp").forEach(el => el.remove());
 Pimp.el=document.createElement("div");
 Pimp.el.classList.add("pimp");
+
+//populate our DOM element:
 Pimp.el.innerHTML=`
   <div class="tabs">
     <div tabindex="0" class="tab debiasbyus off current" title="DeBiasByUs"></div>
@@ -137,59 +211,94 @@ Pimp.el.innerHTML=`
   </div>
   <div class="tabBodies">
     <div class="identity"><a class="identity" href="https://www.biasshield.org/" target="_blank">Bias Shield</a></div>
-    
     <div class="tabBody debiasbyus">
+  
+      <!--debiasbysus: when we haven't detected any translation-->
       <div class="state noTranslation">
         <div class="status">
-          No translation detected.
+          No translation found.
           <span class="substatus">Translate something to see options here for reporting a gender-biased translation.</span>
         </div>
       </div>
+
+      <!--debiasbysus: when the translation is an unsupported language pair-->
       <div class="state unsupportedLanguagPair" style="display: none">
         <div class="status">
           This translation is in an unsupported language pair.
           <span class="substatus">Visit <a href="https://debiasbyus.ugent.be/">DeBiasByUs</a> for a list of languages we support.</span>
         </div>
       </div>
-      <div class="state offerToReport" style="display: none">
+
+      <!--debiasbysus: while we are detecting ambiguities-->
+      <div class="state checking" style="display: none">
         <div class="status">
-          <a class="action submitReport" target="_blank" href="https://debiasbyus.ugent.be/share/">Report a gender-biased translation</a>
-          <a target="_blank" href="https://debiasbyus.ugent.be/learn/">What is a gender-biased translation?</a>
+          <span class="waiter"></span>
+          Checking the translation...
         </div>
       </div>
+
+      <!--debiasbysus: when this translation has already been reported as biased-->
+      <div class="state alreadyReported" style="display: none">
+        <div class="status">
+          <span class="icon warning red"></span> This translation has previously been reported as gender-biased.
+          <span class="action unbias">Unbias</span>
+          <span class="action ununbias">Original</span>
+          <a class="lookupReport" target="_blank" href="#">Details...</a>
+        </div>
+      </div>
+
+      <!--debiasbysus: when this translation has not yet been reported as biased-->
+      <div class="state notYetReported" style="display: none">
+        <div class="status">
+          <span class="icon check grey"></span> This translation has not previously been reported as gender-biased.
+          <a class="action submitReport" target="_blank" href="https://debiasbyus.ugent.be/share/">Report</a>
+          <a target="_blank" href="https://debiasbyus.ugent.be/learn/">About gender bias...</a>
+        </div>
+      </div>
+
     </div>
-    
     <div class="tabBody fairslator" style="display: none">
+
+    <!--fairslator: when we haven't detected any translation-->
       <div class="state noTranslation">
         <div class="status">
-          No translation detected.
+          No translation found.
           <span class="substatus">Translate something to see options here for correcting a biased translation.</span>
         </div>
       </div>
+
+      <!--fairslator: when the translation is an unsupported language pair-->
       <div class="state unsupportedLanguagPair" style="display: none">
         <div class="status">
           This translation is in an unsupported language pair.
-          <span class="substatus">Visit <a href="https://www.fairslator.com/">Fairslator</a> for a list of languages we support.</span><
+          <span class="substatus">Visit <a href="https://www.fairslator.com/">Fairslator</a> for a list of languages we support.</span>
         </div>
       </div>
+
+      <!--fairslator: while we are detecting ambiguities-->
       <div class="state detectingAmbiguities" style="display: none">
         <div class="status">
           <span class="waiter"></span>
           Detecting ambiguities...
         </div>
       </div>
+
     </div>
-    
   </div>
 `;
+
+//attach click events to tabs:
 Pimp.el.querySelectorAll(".tabs > .tab").forEach(el => {
   el.addEventListener("click", function(e){
     if(e.target.classList.contains("debiasbyus")) Pimp.switchTab("debiasbyus");
     if(e.target.classList.contains("fairslator")) Pimp.switchTab("fairslator");
   });
 });
+
+//attach our DOM element to the DOM:
 document.body.style.paddingBottom="400px";
 document.body.appendChild(Pimp.el);
 
+//start checking for screen changes every two seconds: 
 console.log("I'm here.");
 Pimp.check();
